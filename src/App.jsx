bd924b9f785f5dae99f7940e43ee26173564c7b8
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-07-20 20:19:35 +0500

    Adding Tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-xl font-bold text-center'>
+      
+      Weather Finder
+     
+    </div>
+  )
+}
+
+export default App
