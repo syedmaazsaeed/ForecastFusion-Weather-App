d503a496cb58bbb8725b697e9688d9842ff294cc
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-07-22 20:17:56 +0500

    Adding more features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import search from './assets/Icons/search.svg';
+import { useStateContext } from './Context';
 
 function App() {
+  const [input, setInput] = useState('');
+  const { weather, setPlace } = useStateContext();
+
+  console.log(weather);
 
   return (
-    <div className='text-xl font-bold text-center'>
-      
-      Weather Finder 
-     
+    <div className='w-full h-screen text-white px-8'>
+      <nav className='w-full p-3 flex justify-between items-center'>
+        <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          <img src={search} alt="Search Icon" className='w-[1.5rem] h-[1.5rem]'/>
+          <input onKeyUp={(e) => {
+            if(e.key === 'Enter') {
+              // Submit the Form
+              setPlace(input);
+            }
+          }} type="text" className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={ e => setInput(e.target.value)} />
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -1,10 +1,15 @@
+import { useStateContext } from "../Context";
 
 const BackgroundLayout = () => {
+  const { weather } = useStateContext();
+
+  console.log(weather);
+  
   return (
     <div>
-      
+       Background Layout
     </div>
-  )
+  );
 }
 
-export default BackgroundLayout
+export default BackgroundLayout;
